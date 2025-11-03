dee1db1c7d607e8db81872d7bac7e3be1c513a23
Rhaissa de Lima Santos <824133063@ulife.com.br>
2025-11-03 11:50:18 -0300

    Atualição Home e estilização

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import home from './components/home.js';
 
 function App() {
+    const title = "Bem Vindo ao projetode react-bootstrap"
+  const link = [
+
+    {label: 'Cards', href: 'cards.html'},
+    {label: 'Botões', href: 'botoes.html'},
+    {label: 'Formulário', href: 'formulario.html'},
+    {label: 'Navbar', href: 'navbar.html'},
+    {label: 'Layout', href: 'layout.html'},
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <home title={title} links={link}/>
     </div>
   );
 }
